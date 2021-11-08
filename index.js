bec7b512ae025cc314c805d9c4e814bdff8ed08d
robiulislam12 <therobiulislam12@gmail.com>
2021-11-08 12:19:41 +0600

    admin added

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ async function run() {
       await client.connect();
       const database = client.db('doctors_portal');
       const appointmentsCollection = database.collection('appointments');
+      const userCollection = database.collection('users');
 
+      //Appointments GET API
       app.get('/appointments', async (req, res) => {
           const email = req.query.email;
 
@@ -38,14 +40,56 @@ async function run() {
           res.json(appointments);
 
       })
-
+      
+      //Appointment POST API
       app.post('/appointments', async (req, res) => {
           const appointment = req.body;
           const result = await appointmentsCollection.insertOne(appointment);
-          console.log(result);
           res.json(result)
       });
 
+      //User POST API
+      app.post('/users', async(req, res)=>{
+        const user = req.body;
+        const result = await userCollection.insertOne(user)
+        res.json(user)
+      })
+      //Special user get
+      app.get('/users/:email', async(req, res)=>{
+        const email = req.params.email;
+        const query = {email: email};
+        const user = await userCollection.findOne(query)
+        let isAdmin = false;
+        if(user?.role === 'admin'){
+          isAdmin =true
+        }
+        res.json({admin: isAdmin})
+      })
+
+      //User PUT Method
+      app.put('/users', async(req, res)=>{
+        const user = req.body;
+        const filter = {email: user.email};
+        const options = { upsert: true };
+        const updateDoc = {$set: user}
+
+        const result = await userCollection.updateOne(filter, updateDoc, options)
+
+        res.json(result)
+      })
+
+      //Admin Role Update
+      app.put('/users/admin', async (req, res)=>{
+        const user = req.body;
+        // console.log(user.email, 'put')
+        const filter = {email: user.email};
+
+        const updateDoc = {$set: {role: 'admin'}}
+
+        const result = await userCollection.updateOne(filter, updateDoc)
+
+        res.json(result)
+      })
   }
   finally {
       // await client.close();
